3cfe58061979ab70359414588609426c778bbf58
uxname <uxname@gmail.com>
2021-08-12 00:39:41 +0300

    Added ESLint plugin: plugin:@next/next/recommended Changed img tag to next/image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,28 +4,30 @@ import {GetServerSideProps} from 'next';
 import {AllPlanetsDocument, AllPlanetsQuery, AllPlanetsQueryResult, useAllFilmsQuery} from '../generated/graphql';
 import Logo from '../../public/assets/logo.svg';
 import Cat from '../../public/assets/cat.jpg?trace';
+import Image from 'next/image';
 
 export default function IndexPage(props: AllPlanetsQueryResult): ReactNode {
     const {data, loading, error} = useAllFilmsQuery({variables: {first: 1}});
     return (
         <div>
-            <img height={100} width={250} src={Logo}/>
+            <Image height={100} width={250} src={Logo}/>
             <h1>API URL:
                 <a href={process.env.NEXT_PUBLIC_GRAPHQL_API_URL}>{process.env.NEXT_PUBLIC_GRAPHQL_API_URL}</a>
             </h1>
             <h3>SSR Request: </h3>
             <br/>
-            <span>{JSON.stringify(props.data?.allPlanets?.edges)}</span>
+            <pre>{JSON.stringify(props.data?.allPlanets?.edges, null, 2)}</pre>
             <hr/>
             <br/>
             <div>
                 <h3>Client side query:</h3>
                 {
-                    error ? <div>Error</div> : loading ? <div>Loading...</div> : JSON.stringify(data)
+                    error ? <div>Error</div> : loading ? <div>Loading...</div>
+                        : <pre>{JSON.stringify(data, null, 2)}</pre>
                 }
             </div>
-            <img src={Cat.trace}/>
-            <img src={Cat.src}/>
+            <Image src={Cat.trace} width={450} height={300}/>
+            <Image src={Cat.src} width={450} height={300}/>
         </div>
     );
 }
