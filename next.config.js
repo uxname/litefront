07351d00a9ae928f1a8dce986502cae26c165b25
uxname <uxname@gmail.com>
2022-10-30 02:25:30 +0300

    Added sentry support. Removed apollo-server-micro

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPlugins = require('next-compose-plugins');
+const {withSentryConfig} = require('@sentry/nextjs');
 
-module.exports = withPlugins([], {
+const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+    validate: true
+};
+
+module.exports = withPlugins([
+    (nextConfig) => withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+], {
     poweredByHeader: false,
-    distDir: 'build'
+    distDir: 'build',
+    productionBrowserSourceMaps: true
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,38 @@
-import React, {ReactNode, useEffect} from 'react';
+import React, {ReactNode} from 'react';
 import {getApolloClient} from '../utils/ApolloClient';
 import {GetServerSideProps} from 'next';
 import Logo from '../../public/assets/logo.svg';
 import Cat from '../../public/assets/cat.jpg?trace';
 import Image from 'next/image';
-import {DebugDocument, DebugQuery, DebugQueryResult, useEchoMutation} from '../generated/graphql';
+import {AllFilmsDocument, AllFilmsQuery, AllFilmsQueryResult, useAllFilmsQuery} from '../generated/graphql';
 
-export default function IndexPage(props: DebugQueryResult): ReactNode {
-    const [echo, {data, loading, error}] = useEchoMutation({variables: {text: 'Hello World'}});
-    useEffect(() => {
-        echo().then(console.log);
-    }, []);
+export default function IndexPage(props: AllFilmsQueryResult): ReactNode {
+    const {data, loading, error} = useAllFilmsQuery({variables: {take: 1}});
+
+    async function throwTestError() {
+        console.log('Throwing test error');
+        throw new Error('Test error');
+    }
 
     return (
         <div>
             <Image alt={'logo'} height={100} width={250} src={Logo}/>
+            <br/>
+            <button
+                type="button"
+                onClick={async () => {
+                    await throwTestError();
+                }}
+            >
+                Throw error
+            </button>
             <h1>API URL:
                 <a href={process.env.NEXT_PUBLIC_GRAPHQL_API_URL}>{process.env.NEXT_PUBLIC_GRAPHQL_API_URL}</a>
             </h1>
             <h3>SSR Request: </h3>
             <br/>
             {/* eslint-disable-next-line no-magic-numbers */}
-            <pre>{JSON.stringify(props.data?.debug, null, 2)}</pre>
+            <pre>{JSON.stringify(props.data?.allFilms, null, 2)}</pre>
             <hr/>
             <br/>
             <div>
@@ -38,10 +49,10 @@ export default function IndexPage(props: DebugQueryResult): ReactNode {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const echoRes = await getApolloClient.query<DebugQuery>({
-        query: DebugDocument,
+    const echoRes = await getApolloClient.query<AllFilmsQuery>({
+        query: AllFilmsDocument,
         variables: {
-            first: 4
+            take: 2
         }
     });
     return {
