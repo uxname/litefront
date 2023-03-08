b4ec9c055a0e221cf370e5953323b4c3577e4ca8
uxname <uxname@gmail.com>
2023-03-08 12:10:11 +0300

    Added uptime to /_info page

--- src/pages/_info.tsx
+++ src/pages/_info.tsx
@@ -1,12 +1,28 @@
+/* eslint-disable no-magic-numbers */
 import React, { ReactNode } from 'react';
 
 import appInfo from '../../app-info.json';
 
-export default function _InfoPage(): ReactNode {
+export default function InfoPage(props: { uptime: number }): ReactNode {
+  const formatDate = (date: number): string => {
+    const seconds = Math.floor(date % 60);
+    const minutes = Math.floor((date / 60) % 60);
+    const hours = Math.floor((date / (60 * 60)) % 24);
+    const days = Math.floor(date / (60 * 60 * 24));
+
+    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  };
+
   return (
     <div>
       {`${appInfo.name} ${appInfo.version}`}
       <hr />
+      <p>Uptime: {formatDate(props.uptime)}</p>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const uptime = process.uptime();
+  return { props: { uptime } };
 }
