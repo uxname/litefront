434ed4880597be4010f33c8fe2dd61bee1ecc4ac
uxname <uxname@gmail.com>
2023-03-10 16:05:21 +0300

    Added pretty 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import Image from 'next/image';
+import BgImg from '@public/assets/404bg.svg';
+import styled from 'styled-components';
+
+const Page = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const Content = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+`;
 
 export default function Page404() {
   return (
-    <div>
-      <h1>404 not found</h1>
-    </div>
+    <Page>
+      <Image src={BgImg} alt="404" fill />
+      <Content>
+        <h1>404 not found</h1>
+        <p>Sorry, the page you are looking for does not exist.</p>
+      </Content>
+    </Page>
   );
 }
