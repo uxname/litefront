d61b3d898ecd22246d8c0924f6f64c7816b9d0ad
uxname <uxname@gmail.com>
2023-08-10 00:35:54 +0300

    "Implement global error and promise rejection handlers
    Extended the use of React useEffect to catch unhandled errors and promise rejections globally in '_app.tsx' file. This allows the app to log these uncaught exceptions, improving error tracking and debugging efforts."

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { ApolloProvider } from '@apollo/client';
@@ -7,6 +7,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider as StyledComponentProvider } from 'styled-components';
 
 import { getApolloClient } from '@/services/apollo-client.service';
+import { log } from '@/services/log';
 import { themeMui, themeStyled } from '@/theme';
 
 import nextI18NextConfig from '../../next-i18next.config';
@@ -14,6 +15,24 @@ import nextI18NextConfig from '../../next-i18next.config';
 import { ThemeProvider as MaterialUiProvider } from '@mui/material/styles';
 
 function MyApp({ Component, pageProps }: AppProps): ReactNode {
+  useEffect(() => {
+    const handleError = (event: ErrorEvent) => {
+      log.error('Unhandled error', event.error);
+    };
+
+    const handlePromiseRejection = (event: PromiseRejectionEvent) => {
+      log.error('Unhandled promise rejection', event.reason);
+    };
+
+    window.addEventListener('error', handleError);
+    window.addEventListener('unhandledrejection', handlePromiseRejection);
+
+    return () => {
+      window.removeEventListener('error', handleError);
+      window.removeEventListener('unhandledrejection', handlePromiseRejection);
+    };
+  }, []);
+
   return (
     <StyledComponentProvider theme={themeStyled}>
       <MaterialUiProvider theme={themeMui}>
