a1ecfbe6fd02aff4382a049b4bdf650d25bf655a
uxname <uxname@gmail.com>
2024-01-30 10:58:38 +0300

    Dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
 // eslint-disable-next-line eslint-comments/disable-enable-pair
 /* eslint-disable new-cap */
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { configDotenv } from "dotenv";
+import { UserConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({ registerType: "autoUpdate" }),
-    ViteImageOptimizer(),
-  ],
-  build: {
-    sourcemap: true,
-  },
-});
+export default (): UserConfig => {
+  configDotenv();
+
+  return {
+    server: {
+      port: Number(process.env.PORT),
+      strictPort: true,
+    },
+    preview: {
+      port: Number(process.env.PORT),
+      strictPort: true,
+    },
+    plugins: [
+      react(),
+      VitePWA({ registerType: "autoUpdate" }),
+      ViteImageOptimizer(),
+    ],
+    build: {
+      sourcemap: true,
+    },
+  };
+};
