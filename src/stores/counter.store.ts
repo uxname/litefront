d7aa8b562affd386f6ae00e244fd77a9964c7ef0
uxname <uxname@gmail.com>
2024-01-31 12:39:40 +0300

    Added redux devtools

--- src/stores/counter.store.ts
+++ src/stores/counter.store.ts
@@ -1,11 +1,14 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 export interface ICounterStore {
   counter: number;
   increase: () => void;
 }
 
-export const useCounterStore = create<ICounterStore>((set) => ({
-  counter: 0,
-  increase: (): void => set((state) => ({ counter: state.counter + 1 })),
-}));
+export const useCounterStore = create(
+  devtools<ICounterStore>((set) => ({
+    counter: 0,
+    increase: (): void => set((state) => ({ counter: state.counter + 1 })),
+  })),
+);
