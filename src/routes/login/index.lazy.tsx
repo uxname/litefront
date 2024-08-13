58324cf06eec82ae91e06431a3800f6695c84464
uxname <uxname@gmail.com>
2024-08-13 13:57:14 +0300

    Refactored import

--- src/routes/login/index.lazy.tsx
+++ src/routes/login/index.lazy.tsx
@@ -1,6 +1,5 @@
+import { LoginPage } from "@pages/login";
 import { createLazyFileRoute } from "@tanstack/react-router";
-
-import { LoginPage } from "../../pages/login";
 
 export const Route = createLazyFileRoute("/login/")({
   component: () => <LoginPage />,
