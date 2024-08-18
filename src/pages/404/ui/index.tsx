dd80843bc2c0fbe758a07eba1208b86491359a03
uxname <uxname@gmail.com>
2024-08-18 14:56:02 +0300

    Added default translations

--- src/pages/404/ui/index.tsx
+++ src/pages/404/ui/index.tsx
@@ -10,9 +10,14 @@ export const NotFoundPage: React.FC = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>404</h1>
-      <p className={styles.message}>{t("not-found:message")}</p>
+      <p className={styles.message}>
+        {t(
+          "not-found:message",
+          "Oops! The page you are looking for does not exist.",
+        )}
+      </p>
       <Link to={"/"} preload={"viewport"} className={styles.link}>
-        {t("not-found:back-to-home")}
+        {t("not-found:back-to-home", "Back to home")}
       </Link>
     </div>
   );
