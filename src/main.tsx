b358af6961b34c96141a8b6569c2dd80128abc07
uxname <uxname@gmail.com>
2024-08-18 12:11:58 +0300

    Added 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "@generated/routeTree.gen.ts";
+import { NotFoundPage } from "@pages/404";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 
 import "./index.scss";
 import "./app/i18next.ts";
 
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  defaultNotFoundComponent: NotFoundPage,
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
---
+++ src/pages/404/ui/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "@tanstack/react-router";
+
+import styles from "./index.module.scss";
+
+export const NotFoundPage: React.FC = () => {
+  const { t } = useTranslation(["not-found"]);
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.header}>404</h1>
+      <p className={styles.message}>{t("not-found:message")}</p>
+      <Link to={"/"} preload={"viewport"} className={styles.link}>
+        {t("not-found:back-to-home")}
+      </Link>
+    </div>
+  );
+};
